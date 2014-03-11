803ffdc74cc6ecbdd4d3fb3cbf2b851cfc4aca23
encorehu <huyoo353@126.com>
2014-03-11 21:30:33 +0800

    add LoginRequiredMixin

--- home/views.py
+++ home/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+class LoginRequiredMixin(object):
+    '''usage: make sure LoginRequiredMixin comes first.
+
+    #right:
+    class YourNeedLoginView(LoginRequiredMixin, TemplateView):
+        template_name = 'index.html'
+
+    #wrong:
+    class YourNeedLoginView(TemplateView, LoginRequiredMixin):
+        template_name = 'index.html'
+    '''
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
